ec03a9c204dfff5af6912e9013f42091f34fe3b7
magio843 <magio843@yandex.ru>
2022-02-03 21:05:10 +0700

    исправили ошибки

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,7 +2,7 @@
  //Console.SetCursorPosition(10, 4)
  //Console.WriteLine("+")
 
-int xa = 35, ya = 1, xb = 1, yb = 30, xc = 70, yc = 30;
+int xa = 40, ya = 1, xb = 1, yb = 30, xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");    
@@ -13,28 +13,28 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-int x = xa, y = ya;
+int x = xa, y = xb;
 
 int count = 0;
 
-while(count < 10)
+while (count < 10000)
 {
     int what = new Random().Next(0, 3);  //Next(0,3)  0,1,2
     if (what == 0)
     {
-        x = (x+xa)/2;
-        y = (y+ya)/2;
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
     }
         
-    if(what == 1)
+    if (what == 1)
     {
-        x = (x+xb)/2;
-        y = (y+yb)/2;
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
     }
     if (what == 2)
     {
-        x = (x+xc);
-        y = (y+yc);
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
     }
 
     Console.SetCursorPosition(x, y);
