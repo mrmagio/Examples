8bad202f03bb2bc594fe8b61674bd2ad07f28476
magio <magio843@yandex.ru>
2022-02-03 12:22:13 +0700

    add Exampl e005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "сергей")
+{
+    Console.WriteLine("Добро пожаловать - Серёжа!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
